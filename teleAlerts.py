4b17c1631a5a1f6beb899f1a89dbd38d0790f4fa
Leandro Schabarum <leandro.schabarum@renovaretelecom.com.br>
2021-01-28 09:08:21 -0300

    Added docstrings and fixed syntax mistakes

--- teleAlerts.py
+++ teleAlerts.py
@@ -3,23 +3,26 @@ import datetime
 import requests
 
 def confINFO(filename: str):
-	""""""
+	"""Function for creating ConfigParser object holding Telegram Bot information.\n
+	filename (required) ---> str | name with file extension : <filename>.conf"""
 	config = configparser.ConfigParser()
 	config.read(filename)
 
 	return config
 
 def telegram_bot_send_text(info, msg: str):
-	""""""
-	token = info.get('TELEGRAM_chat_info', 'token')
-	chatID = info.get('TELEGRAM_chat_info', 'chatID')
+	"""Function for sending GET request to Telegram Bot API.\n
+	info (required) ---> ConfigParser object\n
+	msg (required) ---> str | message to be sent"""
+	tk = info.get('TELEGRAM_chat_info', 'token')
+	cid = info.get('TELEGRAM_chat_info', 'chatID')
 
-	sendText = f'https://api.telegram.org/bot{token}sendMessage?chat_id={chatID}&parse_mode=Markdown&text={msg}'
-	response = requests.get(sendText)
+	txt = f'https://api.telegram.org/bot{tk}sendMessage?chat_id={cid}&parse_mode=Markdown&text={msg}'
+	response = requests.get(txt)
 
 	return response.json()
 
 if __name__ == '__main__':
-	auth = confINGO('alerts.conf')
+	auth = confINFO('alerts.conf')
 	message = 'test message'
 	telegram_bot_send_text(auth, message)
